ed867f5ca48d993597ff4b05e75f351377a51736
Vallari Agrawal <val.agl002@gmail.com>
2024-09-06 18:49:12 +0530

    Add personal.js and tech.js
    this adds /tech and /personal pages
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,42 @@ import { rhythm } from "../utils/typography"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+  const techPosts = data.techPosts.edges
+  const personalPosts = data.personalPosts.edges
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      {posts.map(({ node }) => {
+      <h1>Programming Blog</h1>
+      {techPosts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug} >
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none`, color: 'black' }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+      <h1>Personal Blogs</h1>
+      {personalPosts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
           <article key={node.fields.slug} >
@@ -49,26 +78,49 @@ const BlogIndex = ({ data, location }) => {
 export default BlogIndex
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
+query {
+  site {
+    siteMetadata {
+      title
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
+  }
+  techPosts: allMarkdownRemark(
+    sort: { fields: [frontmatter___date], order: DESC }
+    filter: { fields: { slug: { regex: "/^\\/tech-/" } } }
+    limit: 5
+  ) {
+    edges {
+      node {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
         }
       }
     }
   }
+  personalPosts: allMarkdownRemark(
+    sort: { fields: [frontmatter___date], order: DESC }
+    filter: { fields: { slug: { regex: "/^\\/personal-/" } } }
+    limit: 5
+  ) {
+    edges {
+      node {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+}
 `
